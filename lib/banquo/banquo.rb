e4d9e1ab72135960fa2730795c2fa063fac2a5a5
pezholio <pezholio@gmail.com>
2013-09-12 20:04:33 +0100

    Tweak for changes to ODC site

--- lib/banquo/banquo.rb
+++ lib/banquo/banquo.rb
@@ -6,7 +6,7 @@ module Banquo
   
   def self.create_certificate(options)
     cmd = `casperjs --web-security=no #{File.dirname(__FILE__)}/script/create-certificate.js #{BASE_URL} #{options[:user].email} #{options[:user].password} #{options[:uri]}`
-    if cmd =~ /#{BASE_URL}\/certificate/ 
+    if cmd =~ /#{BASE_URL}\/datasets/ 
       cmd.strip
     else
       false
